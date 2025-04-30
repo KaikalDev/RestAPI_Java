fcf217646d54d92d97c507f889f0bd2c302ca470
Kaique <kaiquesimoes58@gmail.com>
2025-04-30 01:35:40 -0300

    Init

---
+++ src/main/java/kaique/luan/dev/RestApiJavaApplication.java
@@ -0,0 +1,13 @@
+package kaique.luan.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kaique/luan/dev/RestApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package kaique.luan.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
